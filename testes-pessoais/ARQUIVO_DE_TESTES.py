54a68810835cbd4a5fcc5a1380ad05057c93ed59
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2023-04-27 10:30:12 -0300

    git ignore

--- testes-pessoais/ARQUIVO_DE_TESTES.py
+++ testes-pessoais/ARQUIVO_DE_TESTES.py
@@ -1,12 +1,45 @@
-salario = float(input('Valor a Receber: R$ '))
+import tkinter as tk
+from tkinter import messagebox
 
-impostoDeRenda = 0.11 * salario
-inss = 0.08 * salario
-sindicato = 0.05 * salario
-descontos = impostoDeRenda + inss + sindicato
-salarioLiquido = salario - descontos
-print(f'Salario Bruto R${salario:.2f} \n'
-      f'- IR R${impostoDeRenda :.2f} \n'
-      f'- INSS R${inss :.2f} \n'
-      f'- Sindicato R${sindicato :.2f} \n'
-      f'+ Salario Liquido R${salarioLiquido :.2f}')
+# Lista para armazenar as tarefas
+tasks = []
+
+# Função para adicionar uma nova tarefa
+def add_task():
+    task = task_entry.get()
+    if task:
+        tasks.append(task)
+        task_list.insert(tk.END, task)
+        task_entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Por favor, insira uma tarefa.")
+
+# Função para remover uma tarefa selecionada
+def remove_task():
+    task_index = task_list.curselection()
+    if task_index:
+        task_list.delete(task_index)
+        tasks.pop(task_index[0])
+    else:
+        messagebox.showwarning("Por favor, selecione uma tarefa.")
+
+# Cria a janela principal
+root = tk.Tk()
+root.title("Tarefas")
+
+# Cria os widgets
+task_label = tk.Label(root, text="Task:")
+task_entry = tk.Entry(root)
+add_button = tk.Button(root, text="Add Task", command=add_task)
+remove_button = tk.Button(root, text="Remove Task", command=remove_task)
+task_list = tk.Listbox(root)
+
+# Coloca os widgets na janela
+task_label.grid(row=0, column=0)
+task_entry.grid(row=0, column=1)
+add_button.grid(row=1, column=0)
+remove_button.grid(row=1, column=1)
+task_list.grid(row=2, columnspan=2)
+
+# Inicia o loop do tkinter
+root.mainloop()
