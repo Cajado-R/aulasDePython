bd924a1e9191597d5ffa22b57ada32c0a3b39923
Cláudio Cajado <101744023+Cajado-R@users.noreply.github.com>
2023-05-02 16:11:45 -0300

    Alteração do teste

--- testes-pessoais/ARQUIVO_DE_TESTES.py
+++
@@ -1,5 +0,0 @@
-velocidadeEmMb = 10
-tamanhoDoArquivo = 115
-velocidadeInternet = 10 / 8
-calculoDeTempo = tamanhoDoArquivo / velocidadeInternet
-print(f'Você irá levar: {calculoDeTempo:.0f} segundos para concluir o download.')
